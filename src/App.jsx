531f674004547645ddf31331440bcf0e5eb3f402
Deepak Mardi <deepakmardi22@gmail.com>
2024-01-20 14:46:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Players from "./pages/Players";
-import Contact from "./pages/Contact";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Players from "./pages/Players.jsx";
+import Contact from "./pages/Contact.jsx";
 
 export default function App() {
   return (
