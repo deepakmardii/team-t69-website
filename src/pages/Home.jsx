318014ffe264786969ef5ddf11c8b1ffe2e16279
Deepak Mardi <deepakmardi22@gmail.com>
2024-01-20 14:16:24 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,13 +29,6 @@ const Home = () => {
       <section>
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-              <img
-                alt="Party"
-                src="https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blte2c0dcec8472d272/636476dc2d2a144cc90497fb/GCC_Primer_Article_Header.jpg?width=2472&height=1390"
-                className="absolute inset-0 h-full w-full object-cover"
-              />
-            </div>
             <div className="lg:py-24">
               <h2 className="text-3xl font-bold sm:text-8xl pb-6 uppercase text-red-500">
                 About Us
@@ -62,6 +55,13 @@ const Home = () => {
                   prowess of T69.
                 </p>
               </div>
+            </div>
+            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+              <img
+                alt="Party"
+                src="https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blte2c0dcec8472d272/636476dc2d2a144cc90497fb/GCC_Primer_Article_Header.jpg?width=2472&height=1390"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
             </div>
           </div>
         </div>
